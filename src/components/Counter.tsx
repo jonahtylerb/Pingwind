4b1281de1de845ec5a1816c027730d4fa3972610
Jonah Brupbacher <jonahtylerb@gmail.com>
2023-04-10 18:27:12 -0500

    Make counter simple

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,12 +3,8 @@ import { createSignal } from "solid-js";
 const Counter = () => {
   const [count, setCount] = createSignal(0);
   return (
-    <div class="flex flex-col items-center justify-evenly p-5 w-50 bg-base-200 rounded-xl gap-5">
-      <span class="countdown">
-        <span style={"--value:" + count()} class="text-lg font-bold">
-          {count()}
-        </span>
-      </span>
+    <div class="flex flex-col items-center justify-evenly p-5 w-50 bg-base-200 rounded-$rounded-box gap-5">
+      <span class="font-bold text-4xl">{count()}</span>
       <div class="flex flex-center gap-5">
         <button class="btn" onClick={() => setCount(count() + 1)}>
           +
